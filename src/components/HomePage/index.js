26f19a34773cfd8c871a9847ce0370a9f3c6daee
Nguyen Chau Quyen <69032266+quyen46919@users.noreply.github.com>
2021-10-28 17:06:55 +0700

    Revert "Feature/intro"

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
 import './styles.scss';
 
-
 function HomePage() {
     return (
-        <div >
-            this is home page
+        <div>
+            This is home page
         </div>
     );
 }
