4781159d3b2fca4e9e317fe870737d67a6ce152f
Nguyen Chau Quyen <69032266+quyen46919@users.noreply.github.com>
2021-11-02 09:51:07 +0700

    Revert "Merge pull request #14 from quyen46919/feature/homepage"
    This reverts commit 14a2d0d1e98b9f7efcb973bae3f512baa035b5c2, reversing
    changes made to c8dbfd6af556de2d91f33ae2b8964e6f61ba65eb.

--- src/components/IntroPage/index.js
+++ src/components/IntroPage/index.js
@@ -1,53 +1,10 @@
 import React from 'react';
 import './styles.scss';
-import HeaderTop from '../IntroPage/HeaderTop';
-import Introduce from './Introduce';
-import Feature from './Feature';
-import Infor from './Infor';
-import Content from './Content';
-import Footer from './Footer';
-import imgright from 'assets/images/intro_vienxanh.png';
-import FadeInWhenVisible from './FadeInWhenVisible';
 
 function IntroPage() {
     return (
-        <div className='intro'>
-            <div className="intro__imgright">
-                <FadeInWhenVisible>
-                    <img src={imgright}/>
-                </FadeInWhenVisible>
-            </div>
-            <div className="intro__container">
-                <FadeInWhenVisible>
-                    <div className="intro__headertop">
-                        <HeaderTop/>
-                    </div>
-                </FadeInWhenVisible>
-                <FadeInWhenVisible>
-                    <Introduce/>
-                </FadeInWhenVisible>
-                <FadeInWhenVisible>
-                    <h1 className="intro__title">Các tính năng nổi bật</h1>
-                </FadeInWhenVisible>
-                <div className="intro__feature">
-                    <Feature/>
-                </div>
-                <FadeInWhenVisible>
-                    <p className="intro__title">Về chúng tôi</p>
-                </FadeInWhenVisible>
-                <div className="intro__infor">
-                    <Infor/>
-                </div>
-                <FadeInWhenVisible>
-                    <p className="intro__someimages-text">(*) Some Images and Illustrations belongs to Freepik and Dribbble</p>
-                </FadeInWhenVisible>
-                <div className="intro__content">
-                    <Content/>
-                </div>
-                <FadeInWhenVisible>
-                    <Footer/>
-                </FadeInWhenVisible>
-            </div>
+        <div className="intro">
+            <h1>This is intro page</h1>
         </div>
     );
 }
